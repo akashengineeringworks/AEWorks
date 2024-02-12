0bc0cbade77ff27a98ce13bb9f428121816ff81b
praveen7777 <48596005+17H51A05K9@users.noreply.github.com>
2024-02-12 10:32:45 +0530

    added file src

--- components/Brochures.js
+++ components/Brochures.js
@@ -7,8 +7,8 @@ export default function Brochures() {
     return (
         <>
             <div className="flex flex-wrap gap-6 justify-center bg-gray-300 rounded-lg">
-                <iframe src={Brochure} height="800" width="700" className="md:scale-95 sm:scale-75" />
-                <iframe src={Brochure1} height="800" width="700" className="md:scale-95 sm:scale-75" />
+                <iframe src="files/Brochure.pdf" height="800" width="700" className="md:scale-95 sm:scale-75" />
+                <iframe src="files/Brochure1.pdf" height="800" width="700" className="md:scale-95 sm:scale-75" />
             </div>
         </>
     );
