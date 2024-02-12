9405e60dda14406f67906c8d2be8574451d0ebbd
praveen7777 <48596005+17H51A05K9@users.noreply.github.com>
2024-02-12 10:41:44 +0530

    initial

--- components/Brochures.js
+++ components/Brochures.js
@@ -1,12 +1,13 @@
 import Container from "./container";
-
+import Stationary from "../public/files/StationaryCrane.pdf";
+import OrangePeel from "../public/files/OrangePeelCrab.pdf";
 
 export default function Brochures() {
     return (
         <>
             <div className="flex flex-wrap gap-6 justify-center bg-gray-300 rounded-lg">
-                <iframe src="/files/StationaryCrane.pdf" height="800" width="700" className="md:scale-95 sm:scale-75" />
-                <iframe src="/files/OrangePeelCrab.pdf" height="800" width="700" className="md:scale-95 sm:scale-75" />
+                <embed src={Stationary} height="800" width="700" className="md:scale-95 sm:scale-75" />
+                <embed src={OrangePeel} height="800" width="700" className="md:scale-95 sm:scale-75" />
             </div>
         </>
     );
