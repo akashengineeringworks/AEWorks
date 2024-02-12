bdf32f8e051e35d2d64292f1553509699cca0ebf
praveen7777 <48596005+17H51A05K9@users.noreply.github.com>
2024-02-12 10:12:20 +0530

    add css change

--- components/productsCategories.js
+++ components/productsCategories.js
@@ -45,7 +45,7 @@ const ExploreCard = (props) => {
             </a>
             <div class="p-5 bg-gray-200">
                 <a href="#">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{props.desc}</h5>
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-black">{props.desc}</h5>
                 </a>
             </div>
         </div>
